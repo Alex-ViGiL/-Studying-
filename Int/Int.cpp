0959e0dfb40bda28b5616dea84199287669cd863
Alex-ViGiL <60942937+Alex-ViGiL@users.noreply.github.com>
2020-10-20 12:50:36 +0300

    ::

---
+++ Int/Int.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+
+using namespace std;
+
+int n = 25;
+const int a = 24;
+
+int main()
+{
+
+
+	int n = 5;
+	cout << n<<endl;
+
+	{
+		int n = 10;
+
+		
+		cout << ::n;
+	
+
+
+	}
+
+
+}
