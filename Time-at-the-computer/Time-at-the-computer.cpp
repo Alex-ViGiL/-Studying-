fd1d5c6d54000edd3607c6bcc75de090cdd07183
Alex-ViGiL <aleksejk777a@gmail.com>
2020-10-20 18:32:11 +0300

    2

--- Time-at-the-computer/Time-at-the-computer.cpp
+++ Time-at-the-computer/Time-at-the-computer.cpp
@@ -4,11 +4,29 @@ using namespace std;
 
 //Сколько нужно портратить часов к 100 уровню 
 const int time_to_level_100 = 10000;
-//Всего потрачено времени 
-int Totaltime = 0;
-//Уровень
-int Level = 0;
+//Всего потрачено времени && Уровень 
+int Totaltime(0),Level(0);
 
+void information(int hours, int minutes) {
+
+    system("cls");
+
+    cout << "Информация об пользователе " << endl
+        << "Имя пользователся : " << "Кармазин Алексей " << endl <<
+        "Уровень : " << Level << endl <<
+        "Всего времени потрачено : " << ::Totaltime << endl <<endl<<
+        "Сегодня потрачено времени " <<endl<< "Часов : " << hours << endl <<
+        "Минут : " << minutes;
+
+}
+
+void level_information() {
+
+    if (::Totaltime == 100) {
+        ::Level = 1;
+    };
+
+}
 
 int main()
 {
@@ -16,39 +34,35 @@ int main()
 
     int time(0), hours(0), minutes(0), second(0);
 
-    cout << "Информация об пользователе " << endl
-        << "Имя пользователся : " << "Кармазин Алексей " << endl <<
-        "Уровень : " << Level << endl <<
-        "Всего времени потрачено : " <<::Totaltime<<endl;
+    information(hours,minutes);
 
         for (int i = 0;; i++) {
 
            second = i;
 
-           if (second > 60) {
+           if (second >= 60) {
 
                second = 0;
                i = 0;
                minutes += 1;
+
+               information(hours, minutes);
+               level_information();
                continue;
 
-           }else if (minutes > 60) {
+           }else if (minutes >= 60) {
 
                i = 0;
                minutes = 0;
                hours += 1;
                continue;
-
            }
-
-            Sleep(1000);
-
-            cout <<"Сегодня потрачено времени "<<endl<<
-                "Часов : " <<hours<<endl<<
-                "Минут : "<<minutes<<endl<< 
-                "Секунд : "<<second<<endl;
+           ::Totaltime = hours;
+            Sleep(0);
 
         }
+
+       
     
 }
 
