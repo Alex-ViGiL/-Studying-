ea9ab0778e4b729b81301906ed2092f883e31c83
Alex-ViGiL <60942937+Alex-ViGiL@users.noreply.github.com>
2020-12-08 21:12:41 +0200

    1

--- Int/Int.cpp
+++ Int/Int.cpp
@@ -1,1 +1,23 @@
-﻿
+﻿#include <iostream>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "rus");
+	int mass[20], i(0),sum(0);
+	
+	for (; i < sizeof(mass); i++) {
+		cout << "Введите " << i+1 << " оценку :";
+		cin >> mass[i];
+		sum += mass[i];
+	}
+
+	cout << "Средний бал : " << sum / 20;
+	
+	/*char in_string[500];
+	gets_s(in_string);*/
+
+
+}
---
+++ Lection_11_Pointer/Lection_11.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*int var = 123;
+	int* ptrvar = &var+2002;
+	cout << "&var = " << &var << endl;
+	cout << "ptrvar = " << ptrvar << endl;
+	cout << "var = " << var << endl;
+	cout << "*ptrvar" << *ptrvar << endl;*/
+
+	/*int var = 123;
+	int* ptrvar = &var;
+	int** prt_ptrvar = &ptrvar;
+	int*** ptr_ptr_ptrvar = &prt_ptrvar;
+
+	cout << "var = " << var << endl;
+	cout << "*ptrvar = " << *ptrvar << endl;
+	cout << "**prt_ptrvar = " << **prt_ptrvar << endl;
+	cout << "***ptr_ptr_ptrvar" << ***ptr_ptr_ptrvar << endl;*/
+
+	int value = 7;
+	void* number = &value;
+	int* intPtr = static_cast<int*>(number);
+	cout << *intPtr;
+}
+
+
+
